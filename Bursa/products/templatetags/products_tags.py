4f289da2ee67d2586757b43ba56431593e0e3268
Ramis <Sir.ramis@mail.ru>
2023-03-12 12:42:59 +0300

    Добавил sho_categories

--- Bursa/products/templatetags/products_tags.py
+++ Bursa/products/templatetags/products_tags.py
@@ -5,10 +5,10 @@ register = template.Library()
 
 @register.simple_tag(name='getcats')
 def get_categories(filter=None):
-    if not filter:
-        return Category.objects.all()
-    else:
-        return Category.objects.filter(pk=filter)
+    #if not filter:
+    return Category.objects.all()
+    #else:
+        #return Category.objects.filter(pk=filter)
 
 @register.inclusion_tag('products/list_categories.html')
 def show_categories(sort=None, cat_selected=0):
