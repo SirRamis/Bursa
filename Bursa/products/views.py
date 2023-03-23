db66f90544bbe79b42e38dbb4e0e1c677e99b525
Ramis <Sir.ramis@mail.ru>
2023-03-23 10:02:50 +0300

    added form function 23.03.

--- Bursa/products/views.py
+++ Bursa/products/views.py
@@ -2,10 +2,12 @@ from urllib import request
 
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import *
 from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
-        # {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'},
         ]
@@ -27,11 +29,33 @@ def index(request):
 def about(request):
     return render(request, 'products/about.html', {'menu': menu, 'title': 'О сайте'})
 
+#def addpage(request):
+#    if request.method == 'POST':
+#        form = AddPostForm(request.POST, request.FILES)
+#        if form.is_valid():
+#            print(form.cleaned_data)
+             #form.save()
+             #return redirect('home')
+#    else:
+#        form = AddPostForm()
+#    return render(request, 'product/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 def addpage(request):
-    return render(request, 'products/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
+    if request.method == 'POST':        # это дает возврат заполненных полей
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            try:
+                Products.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, "Ошибка добавления поста")
+    else:
+        form = AddPostForm()
+    return render(request, 'products/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 def contact(request):
     return render(request, 'products/contact.html', {'menu': menu, 'title': 'Мы с вами свяжемся'})
+
 
 def login(request):
     return HttpResponse("Авторизация")
@@ -40,8 +64,8 @@ def login(request):
 def pegeNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request, post_slug):
 
+def show_post(request, post_slug):
     post = get_object_or_404(Products, slug=post_slug)
 
     context = {
@@ -52,16 +76,17 @@ def show_post(request, post_slug):
     }
     return render(request, 'products/post.html', context=context)
 
+
 def show_category(request, cat_id):
     posts = Products.objects.filter(cat_id=cat_id)
-    #cats = Category.objects.all()
+    # cats = Category.objects.all()
     # если пост отсутствует, выводит 404:
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        #'cats': cats,
+        # 'cats': cats,
         'menu': menu,
         'title': 'Отображение по категориям',
         'cat_selected': cat_id,
