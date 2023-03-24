3ccf6b01045c16bc65974d8adf95cdacd99882a4
Ramis <Sir.ramis@mail.ru>
2023-03-24 15:36:56 +0300

    Added Register funcshen

--- Bursa/products/views.py
+++ Bursa/products/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
@@ -135,3 +136,13 @@ class ProductsCategory(DataMixin, ListView):
 #    }
 
  #   return render(request, 'products/index.html', context=context)
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'products/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
